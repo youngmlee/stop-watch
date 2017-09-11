cf772bae2f6a8bb22f43f654eb0ce4906161fec6
Young Lee <ylee8575@gmail.com>
2017-09-11 13:32:35 -0700

    Fixed element name, stored timer value in seconds, created function to prevent start button executing with every click. #1

--- start-watch/main.js
+++ start-watch/main.js
@@ -1,23 +1,11 @@
 var timer = {
   seconds: 0,
-  minutes: 0,
-  hours: 0,
   id: null
 }
 
 function start() {
   timer.id = setInterval(function() {
-    if (timer.seconds !== 60) {
-      timer.seconds++
-    }
-    if (timer.seconds === 60) {
-      timer.seconds = 0
-      timer.minutes++
-    }
-    if (timer.minutes === 60) {
-      timer.minutes = 0
-      timer.hours++
-    }
+    timer.seconds++
   }, 1000)
 }
 
@@ -28,28 +16,39 @@ function stop() {
 function clear() {
   stop()
   timer.seconds = 0
-  timer.minutes = 0
-  timer.hours = 0
 }
 
 function render() {
   setInterval(function() {
-    $seconds = document.querySelector('#seconds')
-    $seconds.textContent = timer.seconds + " seconds"
-    $minutes = document.querySelector('#minutes')
-    $minutes.textContent = timer.minutes + " minutes"
-    $hours = document.querySelector('#hours')
-    $hours.textContent = timer.hours + " hours"
+    if (timer.seconds < 60) {
+      $seconds = document.querySelector('#seconds')
+      $seconds.textContent = timer.seconds + " seconds "
+    }
+    else if (timer.seconds >= 60) {
+      $seconds.textContent = Math.floor(timer.seconds / 60) + " minutes " + (timer.seconds % 60) + " seconds "
+    }
+    else if (timer.seconds >= 3600) {
+      $seconds.textContent = Math.floor(timer.seconds / 3600) + " hours " + Math.floor((timer.seconds % 3600) / 60) + " minutes " + (timer.seconds % 60) + " seconds "
+    }
   }, 1000)
 }
 
+function onetime (node, type, callback) {
+  node.addEventListener(type, function(e) {
+    e.target.removeEventListener(e.type, arguments.callee)
+    return callback(e)
+  })
+}
+
 var $start = document.querySelector('#start')
-$start.addEventListener('click', start)
+onetime($start, 'click', start)
 
 var $stop = document.querySelector('#stop')
 $stop.addEventListener('click', stop)
 
 var $clear = document.querySelector('#clear')
 $clear.addEventListener('click', clear)
+
+var $limitS = document.querySelector('#numberS')
 
 render()
